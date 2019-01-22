0799d01875c2441b3155128b446afe6b3b98e5ec
bre30723 <32175762+bre30723@users.noreply.github.com>
2019-01-22 14:53:45 -0800

    TDD - Failing unit test testWriteOutputFile()

---
+++ src/main/java/com/example/StringSorter/OutputWriter.java
@@ -0,0 +1,12 @@
+package com.example.StringSorter;
+
+import java.io.File;
+import java.util.List;
+
+public class OutputWriter {
+
+    public static File writeOutput(List<String> listToWrite) throws Exception {
+
+        return null;
+    }
+}
--- src/test/java/com/example/StringSorter/StringSorterTest.java
+++ src/test/java/com/example/StringSorter/StringSorterTest.java
@@ -3,10 +3,12 @@ package com.example.StringSorter;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
@@ -14,21 +16,25 @@ public class StringSorterTest {
 
     @Test
     public void testReadInputFile() {
-        List<String> inputList = new ArrayList<String>();
+        List<String> inputList = new ArrayList<>();
 
         inputList = InputReader.processInputFile();
 
         assertThat(inputList, is(notNullValue()));
     }
 
-    @Test
-    public void testSortData() {
-
-    }
 
     @Test
     public void testWriteOutputFile() {
+        List<String>  outputList = new ArrayList<>();
 
+        try {
+            File outputFile = OutputWriter.writeOutput(outputList);
+            assertThat(outputFile, is(notNullValue()));
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 
 }
